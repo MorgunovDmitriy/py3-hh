0fcaa10f63389b364f87c926d1de8fde99018d28
MorgunovDmitriy <137083703+MorgunovDmitriy@users.noreply.github.com>
2023-06-26 14:17:46 +0600

    add comment, o2o,m2o

--- worker/models.py
+++ worker/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Worker(models.Model):
+    user = models.OneToOneField(to=User,
+                                null=True, blank=False,
+                                on_delete=models.CASCADE
+                                # приудалении удалить все связанные поля, можно указать SET_NULL(установить ноль)
+                                )
     name_worker = models.CharField(max_length=255, verbose_name='ФИО')
     speciality = models.CharField(max_length=255, verbose_name='Специальность')
     expested_salary = models.IntegerField(null=True, blank=True, verbose_name='Ожидаемая зарплата')
@@ -9,3 +15,18 @@ class Worker(models.Model):
 
     def __str__(self):
         return self.name_worker
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    to_worker=models.ForeignKey(
+        to=Worker,
+        on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE
+    )
+    def __str__(self):
+        return self.author.username
